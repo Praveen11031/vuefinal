ee3bc4197fd0ca3ac8247552f57fc418a82b1f5f
Praveen11031 <116133645+Praveen11031@users.noreply.github.com>
2023-12-06 21:41:50 -0500

    Add files via upload

---
+++ src/components/PlayerBox.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <div v-for="player in players" :key="player.id">
+      <SinglePlayer v-if="player" :player="player" />
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import SinglePlayer from './SinglePlayer.vue';
+  
+  export default {
+    name: 'PlayerBox',
+    props: {
+      players: Array,
+    },
+    components: {
+      SinglePlayer,
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Your styles go here */
+  </style>
+  
---
+++ src/components/SinglePlayer.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+
+        <p v-if="player">Name:  {{ player.Name }}</p>
+        <p v-if="player">Currentteam:  {{ player.Current }}</p>
+        <p v-if="player">DateOfBirth:  {{ player.Dob }}</p>
+        <p v-if="player">AvalableforAuction:  {{ player.Available }}</p>
+        <p v-if="player">MinimumBiddingprice:  {{ player.Minimumbid }}</p>
+
+                     
+    </div>
+</template>
+
+<script>
+
+export default{
+    name: 'SinglePlayer',
+    props:{
+        player: Array
+    }
+}
+</script>
+
+<style scoped>
+
+.green{
+    background-color: rgb(77, 143, 77);
+}
+
+h4{
+    margin: 2px;
+}
+
+div{
+    background-color: #b48c7e;
+    color: black;
+    padding: 1em;
+    text-align: center;
+    width: 80%;
+    margin: auto;
+    
+
+}
+
+.red{
+background-color: orangered !important;
+
+}
+
+
+</style>
